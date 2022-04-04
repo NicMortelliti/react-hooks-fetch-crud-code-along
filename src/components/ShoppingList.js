5a8d4f138b5b60dd1db3991b0689d7950e0c6df9
NicMortelliti <npmortelliti@gmail.com>
2022-04-04 14:35:40 -0700

    Add if statement to handleUpdateItem to set state for the items list after user adds/removes item from cart

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -28,7 +28,14 @@ function ShoppingList() {
   }, []);
 
   function handleUpdateItem(updatedItem) {
-    console.log("In ShoppingCart: ", updatedItem);
+    const updatedItems = items.map(item => {
+      if (item.id === updatedItem.id) {
+        return updatedItem;
+      } else {
+        return item;
+      }
+    });
+    setItems(updatedItems);
   }
 
   return (
